f0871f128cd4d2f0e2ba52fad2169392eb198048
Marywangare907 <wangariann930@gmail.com>
2025-08-08 14:43:39 +0300

    edited contacts

--- src/appData/personal.tsx
+++ src/appData/personal.tsx
@@ -2,5 +2,5 @@ import { GithubIcon, LinkedIn } from '@/utils/icons'
 
 export const socials = [
   { href: 'https://github.com/Marywangare907/', icon: <GithubIcon /> },
-  { href: 'www.linkedin.com/in/marywangare ', icon: <LinkedIn /> },
+  { href: 'www.linkedin.com/in/marywangare/ ', icon: <LinkedIn /> },
 ]
